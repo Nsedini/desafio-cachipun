7c87e3cca12f94109cccfd998e642835102aa214
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-06-28 14:02:17 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const repetitions = prompt ("Ingrese la cantidad de veces que desea jugar"); // const(variable constante) prompt((input) para pedir datos al usuario) //
+
+const repetitionsQty = Number.parseInt(repetitions) // cambio el contenido de la variable de texto a number
+
+console.log('El usuario quiere que el juego se repita ${repetitionsQty} veces') //immprimimmos en la consola la cantidad de veces
+
+for(let count = 1; count <= repetitionsQty; count = count +1){ //siclo for para realizar el ejercicio repetitionsQty veces con sus 3 condiciones
+    // pedir una respuesta
+    
+    //mostrar el resultado
+}

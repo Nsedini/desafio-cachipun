fa97e9a8a652d617d295ed8e43b27cda7a41ad13
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-06-28 18:05:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const repetitions = prompt ("Ingrese la cantidad de veces que desea jugar"); // const(variable constante) prompt((input) para pedir datos al usuario) //
 
-const repetitionsQty = Number.parseInt(repetitions) // cambio el contenido de la variable de texto a number
+const repetitionsQty = Number.parseInt(repetitions); // cambio el contenido de la variable de texto a number
 
 console.log('El usuario quiere que el juego se repita ${repetitionsQty} veces') //immprimimmos en la consola la cantidad de veces
 
@@ -9,21 +9,30 @@ for(let count = 1; count <= repetitionsQty; count = count +1){ //siclo for para 
     const userMove = prompt("Ingrese su jugada, debe ser: Piedra, Papel o Tijera");
 
     const randomMoveIndex = Math.floor(Math.random() * 3);  /// 1   2    o    3  (elija aleatoriamente)
-    let randomMoveText = "";
-
+    let machineMove = "";
+    
     if (randomMoveIndex === 1) {
-        randomMoveText = "Piedra";
-    } 
-    else if (randomMoveIndex === 2) {
-        randomMoveText = "Papel";
-    } 
-    else if (randomMoveIndex === 3) {
-        randomMoveText = "Tijera";
-    }
-    else {
+        machineMove = "Piedra";
+    } else if (randomMoveIndex === 2) {
+        machineMove = "Papel";
+    } else if (randomMoveIndex === 3) {
+        machineMove = "Tijera";
+    } else {
         console.error('La opcion randomMoveIndex no es valida: ${randomMoveIndex}');
     }
 
-    console.log({randomMoveText, randomMoveIndex})
+    let winner = ""; // Machine - User - Both
+
+    if (machineMove === "Piedra"){
+        if(userMove === "Papel") {
+            winner = "User"
+        } else if (userMove ==="Tijera") {
+            winner = "Machine"
+        } else {
+            winner = "Both"
+        }
+    }
+
+    console.log({machineMove, randomMoveIndex})
     //mostrar el resultado
 }
